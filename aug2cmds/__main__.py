5b43e0c290e30aae5ca62f3aca1eaef0756ae05a
Dominic Cleal <dominic@computerkb.co.uk>
2012-04-02 08:57:17 +0100

    Hook Augtool output class up to main program

--- aug2cmds/__main__.py
+++ aug2cmds/__main__.py
@@ -6,6 +6,7 @@ Designed for use with augtool and Puppet.
 """
 
 import __init__ as aug2cmds
+import outputs
 import argparse
 
 def main():
@@ -21,7 +22,7 @@ augtool/Puppet")
         action='store_const', const='yes',
         help='always take default choices')
     parser.add_argument('-f', '--format',
-        choices=['augtool', 'puppet'],
+        choices=['augtool', 'puppet'], default="augtool",
         help='output format')
     parser.add_argument('path',
         help='filename relative to ROOT to parse')
@@ -30,7 +31,13 @@ augtool/Puppet")
         help='optional Augeas path inside file to process')
     args = parser.parse_args()
 
-    top = aug2cmds.PathNode(args.path, root=args.root, lens=args.lens)
+    pathnode = aug2cmds.PathNode(args.path, root=args.root, lens=args.lens)
+    if args.format == "augtool":
+        output = outputs.Augtool()
+    else:
+        raise RuntimeError("Unknown output format")
+    for cmd in output.visit(pathnode, args.augpath):
+        print cmd
 
 if __name__ == "__main__":
     main()
